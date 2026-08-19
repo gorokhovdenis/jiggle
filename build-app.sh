c561c3bd8544a7e95e572c1873f6a1e2561d89f7
Denis Gorokhov <gorokhovdenis@protonmail.com>
2026-08-19 17:04:45 +0300

    chore: renumber to 0.1
    Starting at 1.x was premature for a project this young; 0.1 is the honest
    number for the first release that actually works everywhere it was tried.

--- build-app.sh
+++ build-app.sh
@@ -54,9 +54,9 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>1.1</string>
+	<string>0.1</string>
 	<key>CFBundleVersion</key>
-	<string>3</string>
+	<string>4</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>13.0</string>
 	<key>NSHumanReadableCopyright</key>
